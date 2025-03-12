573a999d6c238a294ff27c0c01f235505d10f128
Andrei <velikohatkoandrei@gmail.com>
2025-03-12 23:16:23 +0300

    create csharp-project and game functions

---
+++ GuessTheNumberTestsCsharp/GuessTheNumberTestsCsharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTests
+{
+    public class GuessNumber
+    {
+        private int _numberToGuess;
+        private int _attempts;
+
+        public GuessNumber(int? numberToGuess = null)
+        {
+            Random random = new Random();
+            _numberToGuess = numberToGuess ?? random.Next(1, 101);
+            _attempts = 0;
+        }
+
+        public string Guess(int userGuess)
+        {
+            _attempts++;
+            if (userGuess > _numberToGuess)
+            {
+                return "Менше";
+            }
+            else if (userGuess < _numberToGuess)
+            {
+                return "Бiльше";
+            }
+            else
+            {
+                return "Вiрно! Ви вгадали число за " + _attempts + " спроб.";
+            }
+        }
+
+        public int GetAttempts() => _attempts;
+    }
+}
